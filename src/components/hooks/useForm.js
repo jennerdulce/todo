b1fa3ea1ce4475725cc9a7164b11f2e130466258
Jenner Dulce <jennerdulce@gmail.com>
2021-06-09 13:49:23 -0700

    finished custom-Hooks lab

---
+++ src/components/hooks/useForm.js
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+
+const useForm = (callback) => {
+  const [values, setValues] = useState({})
+
+  function handleChange(e) {
+    e.persist();
+    let name = e.target.name;
+    let value = e.target.value;
+    setValues({...values, [name]: value});
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    callback(values)
+  }
+
+  return [handleSubmit, handleChange, values]
+}
+
+export default useForm;
--- src/components/list.js
+++ src/components/list.js
@@ -1,21 +1,59 @@
-import { ListGroup } from 'react-bootstrap';
+import { Toast, Badge, Card, ListGroup } from 'react-bootstrap';
 
 function TodoList(props) {
+
+  let completionStyles = {
+    fontSize: '10px', 
+    padding: '5px',
+    color: 'white', 
+    borderRadius: '15px'
+  }
   return (
-    <ListGroup>
+    <>
       {
         props.list.map(item => (
-          <ListGroup.Item
-            aria-label="listItem"
-            onClick={() => props.handleComplete(item._id)}
-            variant={item.complete ? "success" : "light"}
-            key={item._id}>
-            {item.text}
-          </ListGroup.Item>
+          <Card aria-label="list-item" key={item._id} style={{ width: '40em', marginBottom: '1em' }}>
+            <Card.Header className="header-container">
+              <span 
+              className="pending"
+              onClick={() => props.handleComplete(item._id)}
+              style={{...completionStyles, backgroundColor: item.complete ? 'green' : 'red' }}>
+                {item.complete ? "Completed" : "Pending"}
+              </span>
+              <span> {item.assignee}</span>
+              <span className="deleteItem" value={item.id} onClick={() => props.handleDelete(item._id)}> x</span>
+            </Card.Header>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <div>{item.text}</div>
+                <div className="difficulty">Difficulty: {item.difficulty}</div>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
         ))
       }
-    </ListGroup>
+    </>
   );
 }
 
+
+
+
+
+// {
+//   props.list.map(item => (
+//     <Toast key={item._id}>
+//       <Toast.Header>
+//         <Badge pill
+//           variant={item.complete ? "success" : "danger"}
+//           onClick={() => props.handleComplete(item._id)}
+//         >
+//           {item.complete ? "Completed" : "Pending"}
+//         </Badge>{' '}
+//         <strong className="mr-auto">{item.assignee}</strong>
+//       </Toast.Header>
+//       <Toast.Body>{item.text}<span>{item.difficulty}</span></Toast.Body>
+//     </Toast>
+//   ))
+// }
 export default TodoList
